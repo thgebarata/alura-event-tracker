07dbb236947fb9eb8cd9f1aecf8ae0d772f0ff91
thgebarata <thgebarata@gmail.com>
2022-06-11 22:16:07 -0300

    first commit

---
+++ src/state/hooks/useListaDeEventos.ts
@@ -0,0 +1,8 @@
+import { useRecoilValue } from 'recoil';
+import { eventosFiltradosState } from './../seletores/index';
+
+const useListaDeEventos = () => {
+    return useRecoilValue(eventosFiltradosState)
+}
+
+export default useListaDeEventos;
